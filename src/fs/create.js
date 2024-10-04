56ff1324ed8cc93189ac86eb57254b0974adeb81
90Viktoriya <viktoria.kileeva@gmail.com>
2024-10-04 09:08:19 +0300

    feat: first file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const content = 'I am fresh and young';
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filename = join(__dirname, "files", "fresh.txt");
+    try {
+      await fs.appendFile(filename, content, { flag: 'ax+' });
+    } catch {
+      throw new Error('FS operation failed');
+    }
 };
 
 await create();
