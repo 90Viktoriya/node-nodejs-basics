9e959c05cf49fe2f3b1d32f28d4d8fba036b0ff3
90Viktoriya <viktoria.kileeva@gmail.com>
2024-10-06 12:18:29 +0300

    feat: src/zip part

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import stream from 'stream/promises';
+import { createGzip } from 'zlib';
+import { join } from 'path';
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const input = createReadStream(join(__dirname, "files", "fileToCompress.txt"));
+  const output = createWriteStream(join(__dirname, "files", "archive.gz"));
+  const gzip = createGzip();
+  try {
+    await stream.pipeline(input, gzip, output);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import stream from 'stream/promises';
+import { createUnzip } from 'zlib';
+import { join } from 'path';
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const input = createReadStream(join(__dirname, "files", "archive.gz"));
+  const output = createWriteStream(join(__dirname, "files", "fileToCompress.txt"));
+  const unzip = createUnzip();
+  try {
+    await stream.pipeline(input, unzip, output);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await decompress();
